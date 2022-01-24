e5c8fd26d5863644a458095971eb86847159acb2
NexTPL <aleksander.lyskawa1@gmail.com>
2022-01-24 22:36:39 +0100

    Beta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const def = { AVG: '-', VAR: '-', ST_DEV: '-' };
+	const [List, setList] = useState(def);
+
+	const Transform = (array) => {
+		const REGEX = /^[0-9\s]*$/;
+		if (!REGEX.test(array)) {
+			alert('NaN!');
+			return;
+		}
+		array = array.trim();
+		if (array === '') {
+			setList(def);
+			return;
+		}
+		array = array.split(' ');
+		array = array.filter((n) => n !== '');
+		for (let i in array) {
+			array[i] = Number(array[i]);
+		}
+		const AVG = AVG_Compute(array);
+		const VAR = VAR_Compute(array, AVG);
+		const ST_DEV =
+			Math.round((Math.sqrt(VAR) + Number.EPSILON) * 100) / 100;
+		setList({
+			AVG: AVG,
+			VAR: VAR,
+			ST_DEV: ST_DEV,
+		});
+	};
+
+	const AVG_Compute = (array) => {
+		let sum = 0;
+		for (let i in array) {
+			sum += array[i];
+		}
+		return Math.round((sum / array.length + Number.EPSILON) * 100) / 100;
+	};
+	const VAR_Compute = (array, avg) => {
+		let sum = 0;
+		for (let i in array) {
+			sum += Math.pow(array[i] - avg, 2);
+		}
+		return Math.round((sum / array.length + Number.EPSILON) * 100) / 100;
+	};
+
+	return (
+		<div className='App'>
+			<div className='Title'>
+				<p>Statystyka</p>
+			</div>
+			<div className='Output'>
+				<p>Średnia: {List.AVG}</p>
+				<p>Wariancja: {List.VAR}</p>
+				<p>Odchylenie standardowe: {List.ST_DEV}</p>
+				<input
+					type='text'
+					onChange={(e) => Transform(e.target.value)}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
